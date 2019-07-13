9469f75365daccb83ab4a09f429b4b2c9298129d
Alexander <arv.sander9@gmail.com>
2019-07-13 07:37:13 -0500

    Added Post AddPost to add a set of users

--- src/main/java/com/example/demo/MainController.java
+++ src/main/java/com/example/demo/MainController.java
@@ -1,9 +1,15 @@
 package com.example.demo;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,9 +17,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.example.demo.User;
 import com.example.demo.UserRepository;
 
-
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @Controller //Indica que la clase es un controlador
-@RequestMapping(path="/demo") //Indica la ruta URL del controlador
+@RequestMapping(path="/api") //Indica la ruta URL del controlador
 public class MainController {
 	
 	@Autowired // Para obtener automaticamente el repositorio con el nombre indicado
@@ -32,10 +38,20 @@ public class MainController {
 		
 	}
 	
+	@GetMapping(path="/user/{id}")//Mapeo a GET
+	public @ResponseBody Optional<User> getUser(@PathVariable Integer id){ //Tipo respuesta
+		return userRepository.findById(id);
+	}
+	
 	@GetMapping(path="/all")//Mapeo a GET
-	@Cacheable("users")
 	public @ResponseBody Iterable<User> getAllUsers(){ //Tipo respuesta
 		return userRepository.findAll();
+	}
+	
+	@PostMapping(path="/addPost")
+	public @ResponseBody Iterable<User> addUser(@RequestBody Iterable<User> users) {
+	
+		return userRepository.saveAll(users);
 	}
 
 }
